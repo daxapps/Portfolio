50574ee804bb628032f03a3de92aa428790f5eef
Jason Crawford <daxapps777@gmail.com>
2017-08-30 12:53:02 -0500

    Initial Commit

---
+++ public/js/app.js
@@ -0,0 +1,100 @@
+$(document).ready(function(){
+  
+  // declaring jquery selectors
+  var $window = $(window);
+  var $logo = $('#logo');
+  var $row = $('.row');
+  var $contact = $('#contact-section');
+  var $footer = $('#footer');
+  var $overlay = $('.overlay-container');
+  var $masthead = $('#masthead');
+  
+  $window.on('load', function(){
+    $('#loader-wrapper').fadeOut('slow');
+  });
+  
+  // scrolling effects
+  $window.scroll(function(){
+    // declaring windowscroll
+    var windowScroll = $(this).scrollTop();
+    
+    // masthead parallax effect
+    $logo.css({
+      'transform': 'translate(0px, ' + windowScroll/2 + '%)'
+    });
+   
+    // project section parallax effect
+    $row.each(function(i){
+      // won't trigger until the rows are within 50% of the browser
+      if(windowScroll > $row.eq(i).offset().top - ($window.height() / 1.3)) {
+        if(i % 2 === 0) {
+          $row.eq(i).addClass('evenshow');
+        } else {
+          $row.eq(i).addClass('oddshow');
+        }
+      }
+    });
+    
+    // form fade effect
+    if(windowScroll > $contact.offset().top - ($window.height() / 1.4)) {
+      $('form').css('opacity', '1');
+    }
+    
+    // footer fade effect
+    window.requestAnimationFrame(function(){
+      if(windowScroll > 0) {
+        $footer.fadeIn('slow');
+      } else {
+        $footer.fadeOut('slow');
+      }
+    });
+    
+  });
+  
+  // footer hover animation 
+  $footer.on('mouseenter', function(){
+    $(this).animate({'opacity': '1'}, 500);
+  }).on('mouseleave', function(){
+    $(this).animate({'opacity': '0.5'}, 500);
+  });
+  
+  // footer link for scrolling to contact form
+  $('#scroll-to-contact').click(function(e){
+    e.preventDefault();
+    $('body').animate({scrollTop: $('#contact-section').offset().top}, 1000);
+  });
+  
+  // project section overlay
+  $overlay.on('mouseenter', function(){
+    $(this).children('img').addClass('zoom-in');
+    $(this).children('.overlay').fadeToggle('slow');
+  }).on('mouseleave', function(){
+    $(this).children('img').removeClass('zoom-in');
+    $(this).children('.overlay').fadeToggle('slow');
+  });
+  
+  // Contact Form
+  var email;
+  var name;
+  var content;
+  var message = $('#message');
+  $('#submit').click(function(e){
+    e.preventDefault();
+    email = $('#email').val();
+    name = $('#name').val();
+    content = $('#content').val();
+    if (email === '' || name === '' || content === '') {
+      message.text('One or more fields have been left blank.');
+    } else {
+      message.text('Sending your message...Please wait');
+      $.get('/send', {email: email, name: name, content: email + ' said: ' + content}, function(data){
+        if(data === 'sent') {
+          $('#email').val('');
+          $('#name').val('');
+          $('#content').val('');
+          message.text('Message sent! Thank you for contacting me!');
+        }
+      });
+    }
+  });
+});
---
+++ server.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var path = require('path');
+var sass = require('node-sass-middleware');
+var autoprefixer = require('express-autoprefixer');
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+var filePath = path.join(__dirname, 'public');
+var sassPath = path.join(__dirname, 'sass');
+
+// mailer smtpTransporter configuration
+var transporter = nodemailer.createTransport(smtpTransport({
+  service: 'Gmail',
+  secureConnection: false,
+  port: 587,
+  auth: {
+    user: process.env['GMAIL'],  // need env variables here
+    pass: process.env['GPASS']
+  }
+}));
+
+// set up autoprefixer(this goes before the sass middleware)
+app.use(autoprefixer({ browsers: 'last 2 versions', cascade: false }));
+
+// set up sass middleware to convert scss into css
+app.use(
+  sass({
+    src: sassPath,
+    dest: filePath + '/css',
+    debug: true,
+    outputStyle: 'compressed',
+    prefix: '/css' // crucial line to get the css rendered to the right place
+  })
+);
+
+
+// set up public directory to serve files
+app.use(express.static(filePath));
+
+
+// root path serves up index.html
+app.get('/', function(req, res){
+  res.sendFile('index.html');
+});
+
+// mailer request
+app.get('/send', function(req, res){
+  var mailerOptions = {
+    to: 'daxapps777@gmail.com.com',
+    from: req.query.email,
+    subject: req.query.name,
+    text: req.query.content
+  };
+  console.log(mailerOptions);
+  transporter.sendMail(mailerOptions, function(err, info){
+    if(err){
+      return console.log(err);
+    } 
+    console.log('Message sent successfully! ' + info.response);
+    res.end('sent');
+  });
+});
+
+app.listen(PORT, function(){
+  console.log('Server started on port ' + PORT);
+});
